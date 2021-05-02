114c98b28b28c0da8c01e548a0b6d92863a735e9
Angel ILIEV <a.v.iliev13@gmail.com>
2021-05-02 07:59:05 +0300

    Initial node implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt;
+
+
+#[derive(Debug)]
+struct Node<'a> {
+	name: &'a str,
+}
+
+
+impl<'a> fmt::Display for Node<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		return write!(f, "N[{}]", self.name);
+	}
+}
+
 fn main() {
     println!("Hello, world!");
+    let node = Node{ name: "Angel"};
+    println!("{:#?}", node);
+    println!("{}", node);
 }
